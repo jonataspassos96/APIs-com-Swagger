d976b51919827d8861838827746e99da16f95b62
Jonatas Passos <jpc080496@gmail.com>
2022-07-30 11:11:52 -0300

    feat: create file swaggerConfig

---
+++ src/docs/swagger.config.js
@@ -0,0 +1,15 @@
+const swaggerConfig = {
+  definition: {
+    openapi: '3.0.1',
+    info: {
+      title: 'Express API com Swagger',
+      description: 'Api simples utilizando express documentada pelo swagger',
+      version: '1.0'
+    },
+    server: [{
+      url: 'http://localhost:3334',
+      description: 'servidor local'
+    }]
+  },
+  apis: ['./src/routes/user.routes.js']
+}
